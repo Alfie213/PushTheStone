b8ff6634e42e1387d0ff4d887ebb71d24aec8629
Alfie213 <arturbogoratov@gmail.com>
2023-09-23 14:19:57 +0300

    Added moving by keyboard

--- Assets/Scripts/StoneMover.cs
+++ Assets/Scripts/StoneMover.cs
@@ -1,34 +1,49 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class StoneMover : MonoBehaviour
 {
+    [SerializeField] private float speedByKeyboard;
+    
     private Camera cam;
+    private Rigidbody2D rb;
     
     private bool isHoldingMouse;
     private bool isMouseInScreen;
+    private bool isHoldingKeyboard;
+
+    private float keyboardDelta;
 
     public void OnMoveByMouse(InputAction.CallbackContext context)
     {
-        Debug.Log(context.ReadValue<float>());
-        // if (context.started)
-        // {
-        //     isHoldingMouse = true;
-        // }
-        // else if (context.canceled)
-        // {
-        //     isHoldingMouse = false;
-        // }
+        if (context.started)
+        {
+            isHoldingMouse = true;
+        }
+        else if (context.canceled)
+        {
+            isHoldingMouse = false;
+        }
     }
 
     public void OnMoveByKeyboard(InputAction.CallbackContext context)
     {
-        Debug.Log(context.ReadValue<float>());
+        keyboardDelta = context.ReadValue<float>();
+        if (context.started)
+        {
+            isHoldingKeyboard = true;
+        }
+        else if (context.canceled)
+        {
+            isHoldingKeyboard = false;
+        }
     }
 
     private void Awake()
     {
         cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
@@ -45,14 +60,25 @@ public class StoneMover : MonoBehaviour
 
     private void Update()
     {
-        if (!isHoldingMouse || !isMouseInScreen) return;
-
-        Vector3 mousePos = Input.mousePosition;
-        // mousePos.z = Camera.main.nearClipPlane; Fix z-coordinate.
-        var position = transform.position;
-        position = new Vector3(cam.ScreenToWorldPoint(mousePos).x, position.y,
-            position.z);
-        transform.position = position;
+        if (!isHoldingMouse && !isHoldingKeyboard) return;
+        
+        if (isHoldingMouse && isMouseInScreen)
+        {
+            // Debug.Log("moving by mouse");
+            Vector3 mousePos = Input.mousePosition;
+            // mousePos.z = Camera.main.nearClipPlane; Fix z-coordinate.
+            var position = transform.position;
+            position = new Vector3(cam.ScreenToWorldPoint(mousePos).x, position.y,
+                position.z);
+            transform.position = position;
+        }
+        else if (isHoldingKeyboard)
+        {
+            // Debug.Log("moving by keyboard");
+            Vector2 force = new Vector2(speedByKeyboard * keyboardDelta, 0);
+            rb.AddForce(force);
+            // Debug.Log("force added");
+        }
     }
 
     private void Handle_OnMouseEnterScreen()

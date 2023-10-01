354f0a4b4d20fd8eea3fbac4f1696f44d6d5b847
Alfie213 <arturbogoratov@gmail.com>
2023-10-01 09:19:58 +0300

    Delete unused code

--- Assets/ChunkSpawner.cs
+++ Assets/ChunkSpawner.cs
@@ -9,14 +9,12 @@ public class ChunkSpawner : MonoBehaviour
     private Transform chunkSpawnParent;
 
     private Camera cam;
-    // private Vector3 instantiatePosition;
     private Transform lastInstantiatedChunk;
 
     private void Awake()
     {
         cam = Camera.main;
         lastInstantiatedChunk = GetComponent<Transform>();
-        // instantiatePosition = new Vector3(0f, cam.orthographicSize * 2, 0f);
     }
 
     private void OnEnable()
@@ -33,14 +31,11 @@ public class ChunkSpawner : MonoBehaviour
     {
         for (int i = 0; i < spawnedChunkCount; i++)
             InstantiateRandomChunk();
-
-        // instantiatePosition = new Vector3(0f, cam.orthographicSize * 2, 0f);
     }
 
     private void Handle_OnChunkDestroy()
     {
         InstantiateRandomChunk();
-        // DecreaseInstantiatePosition();
     }
     
     private void InstantiateRandomChunk()
@@ -48,18 +43,5 @@ public class ChunkSpawner : MonoBehaviour
         lastInstantiatedChunk = Instantiate(chunks[Random.Range(0, chunks.Length)],
             lastInstantiatedChunk.position + new Vector3(0f, cam.orthographicSize * 2, 0f), Quaternion.identity,
             chunkSpawnParent).GetComponent<Transform>();
-        // IncreaseInstantiatePosition();
     }
-
-    // private void IncreaseInstantiatePosition()
-    // {
-    //     instantiatePosition.y += cam.orthographicSize * 2;
-    //     Debug.Log(instantiatePosition);
-    // }
-    //
-    // private void DecreaseInstantiatePosition()
-    // {
-    //     instantiatePosition.y -= cam.orthographicSize * 2;
-    //     Debug.Log(instantiatePosition);
-    // }
 }

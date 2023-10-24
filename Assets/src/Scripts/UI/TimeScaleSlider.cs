dcea15b14d4a60b84f537ac74f550327b7a88b01
Alfie213 <arturbogoratov@gmail.com>
2023-10-25 00:22:03 +0300

    Setting default timeScale

--- Assets/src/Scripts/UI/TimeScaleSlider.cs
+++ Assets/src/Scripts/UI/TimeScaleSlider.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Slider))]
 public class TimeScaleSlider : MonoBehaviour
 {
+    private const int DefaultTimeScale = 1;
+    
     private Slider slider;
 
     private void Awake()
@@ -13,16 +15,23 @@ public class TimeScaleSlider : MonoBehaviour
 
     private void OnEnable()
     {
+        SetDefaultTimeScale();
         slider.onValueChanged.AddListener(ChangeTimeScale);
     }
 
     private void OnDisable()
     {
+        SetDefaultTimeScale();
         slider.onValueChanged.RemoveListener(ChangeTimeScale);
     }
 
     private void ChangeTimeScale(float value)
     {
         Time.timeScale = value;
+    }
+
+    private void SetDefaultTimeScale()
+    {
+        Time.timeScale = DefaultTimeScale;
     }
 }

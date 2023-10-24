b04b4ed9b71513c59d236cbf1a62c1e3fc3847ba
Alfie213 <arturbogoratov@gmail.com>
2023-10-24 20:37:34 +0300

    Delete temp code that was causing error

--- Assets/PauseImageButton.cs
+++ Assets/PauseImageButton.cs
@@ -8,19 +8,4 @@ public class PauseImageButton : MonoBehaviour
     {
         EnvironmentEventBus.OnPauseUIClick.Publish();
     }
-
-    private void OnEnable()
-    {
-        EnvironmentEventBus.OnPauseUIClick.Subscribe(dbg);
-    }
-    
-    private void OnDisable()
-    {
-        EnvironmentEventBus.OnPauseUIClick.Unsubscribe(dbg);
-    }
-
-    private void dbg()
-    {
-        Debug.Log("click");
-    }
 }

fa5cd65093f713fdaa78bb99c1b166ace6c5483c
Alfie213 <arturbogoratov@gmail.com>
2023-10-30 14:24:13 +0300

    Change OrderInLayer & BackgroundChanger

---
+++ Assets/src/Scripts/UI/BackgroundChanger.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class BackgroundChanger : MonoBehaviour
+{
+    [SerializeField] private Sprite[] backgrounds;
+    [SerializeField] private SpriteRenderer background;
+    [SerializeField] private Mode mode;
+
+    private enum Mode
+    {
+        RandomEveryGame
+    }
+
+    private void Start()
+    {
+        switch (mode)
+        {
+            case Mode.RandomEveryGame:
+                background.sprite = backgrounds[Random.Range(0, backgrounds.Length)];
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}

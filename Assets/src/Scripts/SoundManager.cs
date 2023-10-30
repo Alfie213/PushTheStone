e20c160af52ef60a49a297964917c501f73b8f87
Alfie213 <arturbogoratov@gmail.com>
2023-10-31 00:42:40 +0300

    Subscriptions

--- Assets/src/Scripts/SoundManager.cs
+++ Assets/src/Scripts/SoundManager.cs
@@ -33,12 +33,26 @@ public class SoundManager : MonoBehaviour
     {
         musicVolumeSlider.onValueChanged.AddListener(Handle_onMusicVolumeChanged);
         effectsVolumeSlider.onValueChanged.AddListener(Handle_onEffectsVolumeChanged);
+        
+        EnvironmentEventBus.OnGameSceneLoad.Subscribe(Handle_OnGameSceneLoad);
+        EnvironmentEventBus.OnGameOver.Subscribe(Handle_OnGameOver);
+        EnvironmentEventBus.OnStoneCollideObstacle.Subscribe(Handle_OnStoneCollideObstacle);
+        EnvironmentEventBus.OnStoneMove.Subscribe(Handle_OnStoneMove);
+        EnvironmentEventBus.OnPowerUpPickUp.Subscribe(Handle_OnPowerUpPickUp);
+        EnvironmentEventBus.OnStoneCollideWall.Subscribe(Handle_OnStoneCollideWall);
     }
 
     private void OnDisable()
     {
         musicVolumeSlider.onValueChanged.RemoveListener(Handle_onMusicVolumeChanged);
         effectsVolumeSlider.onValueChanged.RemoveListener(Handle_onEffectsVolumeChanged);
+        
+        EnvironmentEventBus.OnGameSceneLoad.Unsubscribe(Handle_OnGameSceneLoad);
+        EnvironmentEventBus.OnGameOver.Unsubscribe(Handle_OnGameOver);
+        EnvironmentEventBus.OnStoneCollideObstacle.Unsubscribe(Handle_OnStoneCollideObstacle);
+        EnvironmentEventBus.OnStoneMove.Unsubscribe(Handle_OnStoneMove);
+        EnvironmentEventBus.OnPowerUpPickUp.Unsubscribe(Handle_OnPowerUpPickUp);
+        EnvironmentEventBus.OnStoneCollideWall.Unsubscribe(Handle_OnStoneCollideWall);
     }
 
     private void Handle_onMusicVolumeChanged(float value)
@@ -49,6 +63,36 @@ public class SoundManager : MonoBehaviour
     private void Handle_onEffectsVolumeChanged(float value)
     {
         effectsVolume = value;
+    }
+    
+    private void Handle_OnGameSceneLoad()
+    {
+        
+    }
+    
+    private void Handle_OnGameOver()
+    {
+        
+    }
+
+    private void Handle_OnStoneCollideObstacle()
+    {
+        
+    }
+
+    private void Handle_OnStoneMove()
+    {
+        
+    }
+
+    private void Handle_OnPowerUpPickUp()
+    {
+        
+    }
+    
+    private void Handle_OnStoneCollideWall()
+    {
+        
     }
 
     private void ApplyMusicSettings()

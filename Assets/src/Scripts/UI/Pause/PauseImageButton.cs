e6bd0a901d719b8f20d3c7b2804eb046f2278f9e
Alfie213 <arturbogoratov@gmail.com>
2023-11-03 11:58:39 +0300

    Add delay before unpause

--- Assets/src/Scripts/UI/Pause/PauseImageButton.cs
+++ Assets/src/Scripts/UI/Pause/PauseImageButton.cs
@@ -1,11 +1,20 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Image), typeof(Button))]
 public class PauseImageButton : MonoBehaviour
 {
+    [SerializeField] private float delay;
+    
     public void OnPauseUIClick()
     {
+        StartCoroutine(PublishWithDelay());
+    }
+
+    private IEnumerator PublishWithDelay()
+    {
+        yield return new WaitForSeconds(delay);
         EnvironmentEventBus.OnPauseUIClick.Publish();
     }
 }

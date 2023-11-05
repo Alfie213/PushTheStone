0d4a261153fe9ef26feea29e8fd5baf6cef56689
Alfie213 <arturbogoratov@gmail.com>
2023-11-05 04:24:01 +0300

    Add CollideObstacleAnnihilationHandler

---
+++ Assets/src/Scripts/UI/CollideObstacleAnnihilationHandler.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+public class CollideObstacleAnnihilationHandler : MonoBehaviour
+{
+    [SerializeField] private RectTransform scoreParent;
+    
+    private void OnEnable()
+    {
+        EnvironmentEventBus.OnStoneCollideObstacleAnnihilation.Subscribe(Handle_OnStoneCollideObstacleAnnihilation);
+    }
+
+    private void OnDisable()
+    {
+        EnvironmentEventBus.OnStoneCollideObstacleAnnihilation.Unsubscribe(Handle_OnStoneCollideObstacleAnnihilation);
+    }
+
+    private void Handle_OnStoneCollideObstacleAnnihilation(Vector3 obstaclePosition, int obstacleAnnihilationScore)
+    {
+        Debug.Log("start");
+        Vector3 scorePosition = Camera.main.WorldToScreenPoint(obstaclePosition);
+
+        GameObject scoreGameObject = new GameObject("Score", typeof(RectTransform), typeof(TextMeshProUGUI));
+        scoreGameObject.GetComponent<RectTransform>().position = scorePosition;
+        scoreGameObject.GetComponent<TextMeshProUGUI>().text = obstacleAnnihilationScore.ToString();
+        scoreGameObject.transform.SetParent(scoreParent);
+    }
+}

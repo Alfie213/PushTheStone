e82afafd28366df2bb5decfb2f6d48eed6d5fee8
Alfie213 <arturbogoratov@gmail.com>
2023-11-13 00:18:36 +0300

    End of editing ChunkSpawner*

--- Assets/src/GameSystems/Chunk/Scripts/ChunkSpawner.cs
+++ Assets/src/GameSystems/Chunk/Scripts/ChunkSpawner.cs
@@ -20,6 +20,8 @@ public class ChunkSpawner : MonoBehaviour
     private int lastInstantiatedChunkIndex;
     private int currentCountOfChunks;
 
+    private int missingChunksCount => maxSimultaneousChunks - currentCountOfChunks;
+
     private State currentState;
 
     private enum State
@@ -36,12 +38,10 @@ public class ChunkSpawner : MonoBehaviour
         switch (currentState)
         {
             case State.DefaultSpawning:
-                int missingCountDefaultChunks = maxSimultaneousChunks - currentCountOfChunks;
-                InstantiateRandomChunksNoRepetitions(missingCountDefaultChunks);
+                InstantiateRandomChunksNoRepetitions(missingChunksCount);
                 break;
             case State.AnnihilationSpawning:
-                int missingCountAnnihilationChunks = maxSimultaneousChunks - currentCountOfChunks;
-                InstantiateRandomChunksNoRepetitions(missingCountAnnihilationChunks);
+                InstantiateRandomChunksNoRepetitions(missingChunksCount);
                 break;
             case State.NotSpawning:
                 break;
@@ -103,12 +103,24 @@ public class ChunkSpawner : MonoBehaviour
     
     private void InstantiateRandomChunkNoRepetitions()
     {
+        if (currentState is State.DefaultSpawning)
+        {
+            InstantiateRandomRunningChunkNoRepetitions();
+        }
+        else if (currentState is State.AnnihilationSpawning)
+        {
+            InstantiateRandomAnnihilationChunkNoRepetitions();
+        }
+    }
+
+    private void InstantiateRandomRunningChunkNoRepetitions()
+    {
         int newChunkIndex = Random.Range(0, runningModeChunksPrefabs.Length);
-        while (runningModeChunksPrefabs.Length > 1 && newChunkIndex == lastInstantiatedChunkIndex)
+        while (runningModeChunksPrefabs.Length > 1 && newChunkIndex == lastInstantiatedChunkIndex) // Avoid repetitions if Lenght > 1.
         {
             newChunkIndex = Random.Range(0, runningModeChunksPrefabs.Length);
         }
-
+        
         lastInstantiatedChunk = Instantiate(runningModeChunksPrefabs[newChunkIndex],
             lastInstantiatedChunkTransform.position + new Vector3(0f, cam.orthographicSize * 2, 0f),
             Quaternion.identity,
@@ -116,14 +128,36 @@ public class ChunkSpawner : MonoBehaviour
         lastInstantiatedChunkTransform = lastInstantiatedChunk.transform;
         lastInstantiatedChunkIndex = newChunkIndex;
         currentCountOfChunks++;
-
-        if (currentState is State.AnnihilationSpawning)
+        
+        Obstacle[] obstacleComponents = lastInstantiatedChunk.GetComponentsInChildren<Obstacle>();
+        foreach (Obstacle obstacleComponent in obstacleComponents)
         {
-            Obstacle[] obstacleComponents = lastInstantiatedChunk.GetComponentsInChildren<Obstacle>();
-            foreach (Obstacle obstacleComponent in obstacleComponents)
-            {
-                obstacleComponent.ChangeState(Obstacle.State.Annihilation);
-            }
+            obstacleComponent.ChangeState(Obstacle.State.Default);
+        }
+
+        EnvironmentEventBus.OnChunkInstantiate.Publish(lastInstantiatedChunk);
+    }
+    
+    private void InstantiateRandomAnnihilationChunkNoRepetitions()
+    {
+        int newChunkIndex = Random.Range(0, annihilationModeChunksPrefabs.Length);
+        while (annihilationModeChunksPrefabs.Length > 1 && newChunkIndex == lastInstantiatedChunkIndex) // Avoid repetitions if Lenght > 1.
+        {
+            newChunkIndex = Random.Range(0, annihilationModeChunksPrefabs.Length);
+        }
+        
+        lastInstantiatedChunk = Instantiate(annihilationModeChunksPrefabs[newChunkIndex],
+            lastInstantiatedChunkTransform.position + new Vector3(0f, cam.orthographicSize * 2, 0f),
+            Quaternion.identity,
+            chunkSpawnParent);
+        lastInstantiatedChunkTransform = lastInstantiatedChunk.transform;
+        lastInstantiatedChunkIndex = newChunkIndex;
+        currentCountOfChunks++;
+        
+        Obstacle[] obstacleComponents = lastInstantiatedChunk.GetComponentsInChildren<Obstacle>();
+        foreach (Obstacle obstacleComponent in obstacleComponents)
+        {
+            obstacleComponent.ChangeState(Obstacle.State.Annihilation);
         }
         
         EnvironmentEventBus.OnChunkInstantiate.Publish(lastInstantiatedChunk);
@@ -135,20 +169,20 @@ public class ChunkSpawner : MonoBehaviour
             InstantiateRandomChunkNoRepetitions();
     }
 
-    private void InstantiateRandomChunk()
-    {
-        lastInstantiatedChunk = Instantiate(runningModeChunksPrefabs[Random.Range(0, runningModeChunksPrefabs.Length)],
-            lastInstantiatedChunkTransform.position + new Vector3(0f, cam.orthographicSize * 2, 0f),
-            Quaternion.identity,
-            chunkSpawnParent);
-        lastInstantiatedChunkTransform = lastInstantiatedChunk.transform;
-        currentCountOfChunks++;
-        EnvironmentEventBus.OnChunkInstantiate.Publish(lastInstantiatedChunk);
-    }
-
-    private void InstantiateRandomChunk(int count)
-    {
-        for (int _ = 0; _ < count; _++)
-            InstantiateRandomChunk();
-    }
+    // private void InstantiateRandomChunk()
+    // {
+    //     lastInstantiatedChunk = Instantiate(runningModeChunksPrefabs[Random.Range(0, runningModeChunksPrefabs.Length)],
+    //         lastInstantiatedChunkTransform.position + new Vector3(0f, cam.orthographicSize * 2, 0f),
+    //         Quaternion.identity,
+    //         chunkSpawnParent);
+    //     lastInstantiatedChunkTransform = lastInstantiatedChunk.transform;
+    //     currentCountOfChunks++;
+    //     EnvironmentEventBus.OnChunkInstantiate.Publish(lastInstantiatedChunk);
+    // }
+    //
+    // private void InstantiateRandomChunk(int count)
+    // {
+    //     for (int _ = 0; _ < count; _++)
+    //         InstantiateRandomChunk();
+    // }
 }
